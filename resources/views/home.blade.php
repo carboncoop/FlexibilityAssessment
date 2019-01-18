cac38f9e58582eeb0dd83d5ecfa9291a2f3fedfe
cagabi <carlos@carbon.coop>
2019-01-18 13:35:36 +0000

    Makes linkable the whole buttons in the home page

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -18,8 +18,9 @@
         vertical-align: top;
     }
 
-    #home button a{    
+    #home button{    
         color:#ff2712;
+        cursor:pointer;
     }
 
     #home button a:hover{    
@@ -39,13 +40,13 @@
         <p>We are developing a project including a 'demonstration' or research web platform to help community groups and residents understand the coming changes in the energy system in Scotland and the UK. The project also helps to show how these changes might help community groups and residents to reduce fuel costs or earn an income from getting involved in the energy market. We want to test this platform to help us understand what information you (either as a community organisation representative or as a resident) would find helpful to understanding the changes and the likely opportunities. </p>
 
         <div id="navigation-buttons">
-            <button><a href="more-about-the-project">More about the project</a></button>        
+            <a href="more-about-the-project"><button>More about the project</button></a>
 
-            <button><a href="how-could-I-benefit-from-flexibility">How could I benefit from 'flexibility'?</a></button>        
+            <a href="how-could-I-benefit-from-flexibility"><button>How could I benefit from 'flexibility'?</button></a>
 
-            <button><a href="how-could-my-organisation-benefit-from-flexibility">How could my organisation benefit from 'flexibility'?</a> </button>
+            <a href="how-could-my-organisation-benefit-from-flexibility"><button>How could my organisation benefit from 'flexibility'?</button></a> 
 
-            <button><a href="scotland-potential-flexibility-map">Scotland potential flexibility map</a> </button>
+            <a href="scotland-potential-flexibility-map"><button>Scotland potential flexibility map</button></a> 
 
         </div>
 
